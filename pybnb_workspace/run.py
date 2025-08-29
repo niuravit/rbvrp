d6ab6286e86e6240a0d8814171da6a8841245033
NiuRavit <niu.ravit@gmail.com>
2025-08-29 19:27:51 -0400

    add gurobi license path

--- pybnb_workspace/run.py
+++ pybnb_workspace/run.py
@@ -1,7 +1,7 @@
 # MODULE_DIR = '/Users/ravitpichayavet/Documents/GaTechIE/GraduateResearch/CTC_CVRP/Modules'
 MODULE_DIR = '../Modules'
 # GUROBI_LICENSE_DIR = '/Users/ravitpichayavet/gurobi.lic'
-# os.environ['GRB_LICENSE_FILE'] = GUROBI_LICENSE_DIR
+# PACE GUROBI = '/usr/local/pace-apps/manual/packages/gurobi/11.0.1/license/gurobi.lic'
 MAIN_DIR = '../ComputationalExperiment/'
 
 ARG_COLOR = '#104375'
@@ -94,12 +94,23 @@ parser.add_argument(
     help="Path to the visualization configuration JSON file. Defaults to 'vis_config.json'."
 )
 
-
+parser.add_argument(
+    "--gurobi-license",
+    type=str,
+    default="LOCAL",
+    help="Path to the Gurobi license file. Defaults to 'gurobi.lic'."
+)
 try:
     args = parser.parse_args()
 except argparse.ArgumentError as e:
     print(f"Error parsing arguments: {e}")
     sys.exit(1)
+
+if (args.gurobi_license == "LOCAL"):
+    os.environ['GRB_LICENSE_FILE'] = '/Users/ravitpichayavet/gurobi.lic'
+elif (args.gurobi_license == "PACE"):
+    os.environ['GRB_LICENSE_FILE'] = '/usr/local/pace-apps/manual/packages/gurobi/11.0.1/license/gurobi.lic'
+
 
 # Step 4: Use the parsed arguments in your script's logic
 print(f"Starting experiment with:")
@@ -108,6 +119,7 @@ print(f"-- Working directory: {args.working_dir}")
 print(f"-- Instance config: {args.instance_config}")
 print(f"-- Experiment config: {args.experiment_config}")
 print(f"-- Visualization config: {args.vis_config}")
+print(f"-- Gurobi license: {args.gurobi_license}")
 
 instance_config = util.read_json(
     file_path=f'{args.config_dir}/instance_configs/{args.instance_config}')
